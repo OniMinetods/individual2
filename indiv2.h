33e9967c8d84a11576f7639f83481baaa9a165c8
OniMinetods <146983705+OniMinetods@users.noreply.github.com>
2025-04-17 14:00:17 +0300

    feat: created class Dish

--- indiv2.h
+++ indiv2.h
@@ -1,1 +1,15 @@
-#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Dish {
+private:
+	string dishName;
+	double price;
+public:
+	Dish();
+	Dish(string _name, double _price);
+
+	string getName();
+	double getPrice();
+};
